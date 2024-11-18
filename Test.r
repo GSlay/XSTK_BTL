bc00a8cc95042b2d1b9652e518a18bce4b3561d6
GSlay <mha220871@gmail.com>
2024-11-18 07:55:15 +0700

    update Test.r

--- Test.r
+++ Test.r
@@ -32,12 +32,8 @@ GPU_data$height <- sapply(GPU_data$res_split, function(x) as.numeric(x[2]))
 # Tính number_of_pixels
 data$number_of_pixels <- GPU_data$width * GPU_data$height
 
-# Tách cột Best_Resolution thành hai phần: width và heights
-GPU_data$res_split <- strsplit(GPU_data$Best_Resolution, " x ")
-GPU_data$width <- sapply(GPU_data$res_split, function(x) as.numeric(x[1]))
-GPU_data$height <- sapply(GPU_data$res_split, function(x) as.numeric(x[2]))
-# Tính number_of_pixels
-data$number_of_pixels <- GPU_data$width * GPU_data$height
+# Chuyển Manufacturer thành dạng factor
+data$Manufacturer <- as.factor(data$Manufacturer)
 
 cat("\n\n-- Kiểm tra số lượng và tỷ lệ dữ liệu khuyết --")
 library(inspectdf)
